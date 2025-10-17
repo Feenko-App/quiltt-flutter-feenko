8ad72b0ee3bed0fe826d8fb89f46c354d8e367c3
kyrylokharchenko <kharchenko.kir@gmail.com>
2025-10-17 15:06:09 +0300

    [] Fix system gesture insets calculation
    - Corrected view padding calculation for bottom system gestures
    - Improved compatibility with iOS device layouts

--- lib/quiltt_connector.dart
+++ lib/quiltt_connector.dart
@@ -56,7 +56,7 @@ class QuilttConnector {
   }
 
   void _showWebView(BuildContext context, QuilttConnectorConfiguration config) {
-    final viewPadding = MediaQuery.of(context).viewInsets.bottom;
+    final viewPadding = MediaQuery.of(context).systemGestureInsets.bottom;
 
     cupertino.showCupertinoSheet(
       useNestedNavigation: true,
